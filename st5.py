59ccceae78cbf824b133ab397b8b89c66deafebb
Gopalika Sharma <54693803+gsharma14@users.noreply.github.com>
2021-11-01 13:47:00 -0400

    Add files via upload

--- st5.py
+++ st5.py
@@ -1,6 +1,20 @@
 import st7
 import st6
 import streamlit as st
+import pandas as pd
+import sklearn
+from sklearn.svm import SVC
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neural_network import MLPClassifier
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import plot_confusion_matrix, plot_roc_curve, plot_precision_recall_curve
+from sklearn.metrics import precision_score, recall_score
+import os
 PAGES = {
     "Classification Explanatory Tool": st7,
     "Prediction App": st6
